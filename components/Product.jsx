e0d4ffb04281e936d5cd6b7be5553f05ce325943
Paulin Kubwira <95833695+ThePaulin@users.noreply.github.com>
2022-08-17 12:48:15 -0400

    Update Product.jsx
    Added dollar sign fix

--- components/Product.jsx
+++ components/Product.jsx
@@ -16,7 +16,7 @@ const Product = ({product: {image, name,slug, price}}) => {
             />
 
           <p className='product-name'>{name}</p>
-          <p className='product-price'>`${price}$`</p>
+          <p className='product-price'>${price}</p>
         </div>
         
 
